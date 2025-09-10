ed1834e3303ab89391107d17255e202e6dac67c4
Rugsoft <futuroelectronico@gmail.com>
2025-09-10 16:55:27 +0200

    Cambio de un numero

--- src/FigurasGeometricas/Main.java
+++ src/FigurasGeometricas/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Circulo circulo = new Circulo(5);
-		Rectangulo rectangulo = new Rectangulo(4,6);
+		Rectangulo rectangulo = new Rectangulo(4,5.5);
 		
 		circulo.calcularArea();
 		circulo.calcularPerimetro();
