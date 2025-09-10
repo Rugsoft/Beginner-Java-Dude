3ce3f23204dc478b0647e350fec6b68bc1dd6a48
Rugsoft <futuroelectronico@gmail.com>
2025-09-10 13:20:59 +0200

    cambiado totat

--- src/Calculadora/Calculadora.java
+++ src/Calculadora/Calculadora.java
@@ -4,26 +4,28 @@ public class Calculadora {
 	public Calculadora(double numero1, double numero2) {
 		this.numero1 = numero1;
 		this.numero2 = numero2;
+		this.total = 0;
 	}
 	double numero1;
 	double numero2;
+	double total;
 	
 	public void sumar() {
 		
-		double total = numero1 + numero2;
+		total = numero1 + numero2;
 		System.out.println("La suma de "+numero1+" y "+numero2+" es: "+total);
 	}
 	
 	public void restar() {
 		
-		double total = numero1 - numero2;
+		total = numero1 - numero2;
 		System.out.println("La resta de "+numero1+" y "+numero2+" es: "+total);
 		
 	}
 	
 	public void multiplicar() {
 		
-		double total = numero1 * numero2;
+		total = numero1 * numero2;
 		System.out.println("La multiplicacion de "+numero1+" y "+numero2+" es: "+total);
 		
 	}
@@ -35,7 +37,7 @@ public class Calculadora {
 			System.out.println("No se puede dividir por cero");
 		}else {
 			
-			double total = numero1 / numero2;
+			total = numero1 / numero2;
 			System.out.println("La division de "+numero1+" y "+numero2+" es: "+total);
 		}
 		
